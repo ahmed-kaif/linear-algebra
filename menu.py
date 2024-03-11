8c50168d837e049ee9c5d8dd8e21e1226cc87561
ahmed-kaif <kaifkhan.rcs@gmail.com>
2024-03-11 08:39:53 +0600

    Rich module added:

--- menu.py
+++ menu.py
@@ -1,12 +1,23 @@
 import os
-menu = '''Select Option
+from rich.prompt import Prompt 
+from rich.console import Console
+from rich.markdown import Markdown
+menu = '''# Linear Algebra Calculator
+Select Option
 1. Rank of Matrix
 2. EigenValues & EigenVectors
+3. Quit
 '''
-print(menu)
-option = int(input("Enter your choice: "))
-
-if option == 1:
-    os.system("python rank.py")
-elif option == 2:
-    os.system("python eigen.py")
+md = Markdown(menu)
+console = Console()
+console.print(md)
+option = Prompt.ask("Enter your choice",choices=["1","2","3"])
+try:
+    if option == "1":
+        os.system("python rank.py")
+    elif option == "2":
+        os.system("python eigen.py")
+    elif option == "3":
+        exit(0)
+except:
+    print("Bye")
